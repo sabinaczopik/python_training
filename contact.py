3b778557ff0843dcac6df8d33ce5a08703a2c268
Sabina <sabina.czopik@stxnext.pl>
2017-02-13 11:32:35 +0100

    new contact class

---
+++ contact.py
@@ -0,0 +1,11 @@
+class Contact:
+
+    def __init__(self,first_name, last_name, company, address, phone_home, e_mail, year, address_2):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.company = company
+        self.address = address
+        self.phone_home = phone_home
+        self.e_mail = e_mail
+        self.year = year
+        self.address_2 = address_2
