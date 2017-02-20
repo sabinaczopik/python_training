128820f0ba6c5caff56fccf4ea324030024ab736
Sabina <sabina.czopik@stxnext.pl>
2017-02-20 14:00:03 +0100

    test with fixtura and pytest

---
+++ application.py
@@ -0,0 +1,99 @@
+from selenium.webdriver.firefox.webdriver import WebDriver
+
+class Application:
+
+    def __init__(self):
+        self.wd = WebDriver()
+        self.wd.implicitly_wait(60)
+
+    def log_out(self):
+        wd = self.wd
+        wd.find_element_by_link_text("Logout").click()
+
+    def return_to_groupe_page(self):
+        wd = self.wd
+        wd.find_element_by_link_text("group page").click()
+
+    def return_to_homepage(self):
+        wd = self.wd
+        wd.find_element_by_link_text("home page").click()
+
+    def create_new_group(self, group):
+        wd = self.wd
+        self.open_groups()
+        # init group creations
+        wd.find_element_by_name("new").click()
+        # fill group form
+        wd.find_element_by_name("group_name").click()
+        wd.find_element_by_name("group_name").clear()
+        wd.find_element_by_name("group_name").send_keys(group.name)
+        wd.find_element_by_name("group_header").click()
+        wd.find_element_by_name("group_header").clear()
+        wd.find_element_by_name("group_header").send_keys(group.header)
+        wd.find_element_by_name("group_footer").click()
+        wd.find_element_by_name("group_footer").clear()
+        wd.find_element_by_name("group_footer").send_keys(group.footer)
+        # submit group creations
+        wd.find_element_by_name("submit").click()
+        self.return_to_groupe_page()
+
+    def create_contact(self, contact):
+        wd = self.wd
+        # init contact creations
+        wd.find_element_by_link_text("add new").click()
+        # fill contact form
+        wd.find_element_by_name("firstname").click()
+        wd.find_element_by_name("firstname").clear()
+        wd.find_element_by_name("firstname").send_keys(contact.first_name)
+        wd.find_element_by_name("middlename").click()
+        wd.find_element_by_name("lastname").click()
+        wd.find_element_by_name("lastname").clear()
+        wd.find_element_by_name("lastname").send_keys(contact.last_name)
+        wd.find_element_by_name("company").click()
+        wd.find_element_by_name("company").clear()
+        wd.find_element_by_name("company").send_keys(contact.company)
+        wd.find_element_by_name("address").click()
+        wd.find_element_by_name("address").clear()
+        wd.find_element_by_name("address").send_keys(contact.address)
+        wd.find_element_by_name("home").click()
+        wd.find_element_by_name("home").clear()
+        wd.find_element_by_name("home").send_keys(contact.phone_home)
+        wd.find_element_by_name("email").click()
+        wd.find_element_by_name("email").clear()
+        wd.find_element_by_name("email").send_keys(contact.e_mail)
+        # choose birth date
+        if not wd.find_element_by_xpath("//div[@id='content']/form/select[1]//option[13]").is_selected():
+            wd.find_element_by_xpath("//div[@id='content']/form/select[1]//option[13]").click()
+        if not wd.find_element_by_xpath("//div[@id='content']/form/select[2]//option[2]").is_selected():
+            wd.find_element_by_xpath("//div[@id='content']/form/select[2]//option[2]").click()
+        wd.find_element_by_name("byear").click()
+        wd.find_element_by_name("byear").clear()
+        wd.find_element_by_name("byear").send_keys(contact.year)
+        wd.find_element_by_name("address2").click()
+        wd.find_element_by_name("address2").clear()
+        wd.find_element_by_name("address2").send_keys(contact.address_1)
+        # submit contact creations
+        wd.find_element_by_xpath("//div[@id='content']/form/input[21]").click()
+
+    def open_groups(self):
+        wd = self.wd
+        wd.find_element_by_link_text("groups").click()
+
+    def log_in(self, username, password):
+        wd = self.wd
+        self.open_home_page()
+        wd.find_element_by_id("content").click()
+        wd.find_element_by_name("user").click()
+        wd.find_element_by_name("user").clear()
+        wd.find_element_by_name("user").send_keys(username)
+        wd.find_element_by_name("pass").click()
+        wd.find_element_by_name("pass").clear()
+        wd.find_element_by_name("pass").send_keys(password)
+        wd.find_element_by_xpath("//form[@id='LoginForm']/input[3]").click()
+
+    def open_home_page(self,):
+        wd = self.wd
+        wd.get("http://localhost/addressbook/group.php")
+
+    def destroy(self):
+        self.wd.quit()
--- test_add_group.py
+++ test_add_group.py
@@ -1,79 +1,21 @@
 # -*- coding: utf-8 -*-
-from selenium.webdriver.firefox.webdriver import WebDriver
-import unittest
+import pytest
 from group import Group
-from contact import Contact
+from application import Application
 
-def is_alert_present(wd):
-    try:
-        wd.switch_to_alert().text
-        return True
-    except:
-        return False
 
-class test_add_group(unittest.TestCase):
-    def setUp(self):
-        self.wd = WebDriver()
-        self.wd.implicitly_wait(60)
+@pytest.fixture
+def app(request):
+    fixtura = Application()
+    request.addfinalizer(fixtura.destroy)
+    return fixtura
 
-    def test_add_group(self):
-        wd = self.wd
-        self.open_home_page(wd)
-        self.log_in(wd, username="admin", password="secret")
-        self.open_groups(wd)
-        self.create_new_group(wd, Group(name="test", header="test2", footer="test3"))
-        self.return_to_groupe_page(wd)
-        self.log_out(wd)
+def test_add_group(app):
+    app.log_in(username="admin", password="secret")
+    app.create_new_group(Group(name="test", header="test2", footer="test3"))
+    app.log_out()
 
-    def test_empty_group(self):
-        wd = self.wd
-        self.open_home_page(wd)
-        self.log_in(wd, username="admin", password="secret")
-        self.open_groups(wd)
-        self.create_new_group(wd,Group(name="", header="", footer=""))
-        self.return_to_groupe_page(wd)
-        self.log_out(wd)
-
-    def log_out(self, wd):
-        wd.find_element_by_link_text("Logout").click()
-
-    def return_to_groupe_page(self, wd):
-        wd.find_element_by_link_text("group page").click()
-
-    def create_new_group(self, wd, group):
-        # init group creations
-        wd.find_element_by_name("new").click()
-        # fill group form
-        wd.find_element_by_name("group_name").click()
-        wd.find_element_by_name("group_name").clear()
-        wd.find_element_by_name("group_name").send_keys(group.name)
-        wd.find_element_by_name("group_header").click()
-        wd.find_element_by_name("group_header").clear()
-        wd.find_element_by_name("group_header").send_keys(group.header)
-        wd.find_element_by_name("group_footer").click()
-        wd.find_element_by_name("group_footer").clear()
-        wd.find_element_by_name("group_footer").send_keys(group.footer)
-        # submit group creations
-        wd.find_element_by_name("submit").click()
-
-    def open_groups(self, wd):
-        wd.find_element_by_link_text("groups").click()
-
-    def log_in(self, wd, username, password):
-        wd.find_element_by_id("content").click()
-        wd.find_element_by_name("user").click()
-        wd.find_element_by_name("user").clear()
-        wd.find_element_by_name("user").send_keys(username)
-        wd.find_element_by_name("pass").click()
-        wd.find_element_by_name("pass").clear()
-        wd.find_element_by_name("pass").send_keys(password)
-        wd.find_element_by_xpath("//form[@id='LoginForm']/input[3]").click()
-
-    def open_home_page(self, wd):
-        wd.get("http://localhost/addressbook/group.php")
-
-    def tearDown(self):
-        self.wd.quit()
-
-if __name__ == '__main__':
-    unittest.main()
+def test_empty_group(app):
+    app.log_in(username="admin", password="secret")
+    app.create_new_group(Group(name="", header="", footer=""))
+    app.log_out()
