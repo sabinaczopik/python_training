5942b104adf7eda8ac41be0768e83f9596ab88be
Sabina <sabina.czopik@stxnext.pl>
2017-03-06 10:09:28 +0100

    compare group list

--- test/test_edit_group.py
+++ test/test_edit_group.py
@@ -4,11 +4,16 @@ from model.group import Group
 def test_edit_first_group_name(app):
     if app.group.count() == 0:
         app.group.create(Group(name="test", header="test", footer= "footer group"))
+    old_groups = app.group.get_group_list()
     app.group.edit_first_group(Group(name="New Group"))
-
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) == len(new_groups)
 
 
 def test_edit_first_group_header(app):
     if app.group.count() == 0:
         app.group.create(Group(name="test", header="test", footer= "footer group"))
+    old_groups = app.group.get_group_list()
     app.group.edit_first_group(Group(header="New header"))
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) == len(new_groups)
