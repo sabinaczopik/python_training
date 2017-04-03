b76e0d843154436975b500544014f8eca4e8ed8e
Sabina <sabina.czopik@stxnext.pl>
2017-04-03 14:09:39 +0200

    add test data from json

---
+++ data/contacts.py
@@ -0,0 +1,6 @@
+from model.contact import Contact
+
+testdata = [
+    Contact(first_name="first_name1", last_name="last_name1", company="company1"),
+    Contact(first_name="first_name2", last_name="last_name2", company="company2")
+]
--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -1,25 +1,10 @@
 # -*- coding: utf-8 -*-
 
 from model.contact import Contact
-import pytest
-import random
-import string
 
 
-def random_string(prefix, maxlen):
-    symbols = string.ascii_letters + string.digits + string.punctuation + " "*10
-    return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
-
-
-testdata = [Contact(first_name="", last_name="", company="")] + [
-        Contact(first_name=random_string("first_name", 10), last_name=random_string("last_name", 20),
-                company=random_string("company", 20))
-        for i in range(5)
-    ]
-
-
-@pytest.mark.parametrize('contact', testdata, ids=[repr(x) for x in testdata])
-def test_add_contact(app, contact):
+def test_add_contact(app, json_contacts):
+    contact = json_contacts
     old_contacts = app.contact.get_contact_list()
     app.contact.create(contact)
     assert len(old_contacts) + 1 == app.contact.count()
