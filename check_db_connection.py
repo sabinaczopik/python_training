db71b9ff83232e1d3d3c2aba0ebdceafabe358fc
Sabina <sabina.czopik@stxnext.pl>
2017-04-06 11:55:22 +0200

    limitations or select conditions -contact

--- check_db_connection.py
+++ check_db_connection.py
@@ -1,11 +1,12 @@
-import mysql.connector
 
-connection = mysql.connector.connect(host="127.0.0.1", database="addressbook", user="root", password="")
+from fixture.db import DbFixture
+
+db = DbFixture(host="127.0.0.1", name="addressbook", user="root", password="")
 
 try:
-    cursor = connection.cursor()
-    cursor.execute("select * from group_list")
-    for row in cursor.fetchall():
-        print(row)
+    contacts = db.get_contact_list()
+    for contact in contacts:
+        print(contact)
+    print(len(contacts))
 finally:
-    connection.close()
+    db.destroy()
